db8ea39c0b08af4fb6a1cce24f4114eb2f21315f
Brunojoe1994 <brunojoe040695@gmail.com>
2026-05-11 21:43:01 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const PREFIX = 'v1';let i = 0;self.addEventListener('install', function (event) {self.skipWaiting();});self.addEventListener('activate', function (event) {clients.claim();});self.addEventListener('fetch', function (event) {i++;});
