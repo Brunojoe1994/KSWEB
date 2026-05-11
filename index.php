f137633aa8a139b9fd334c66ea2dcea64409757b
Brunojoe1994 <brunojoe040695@gmail.com>
2026-05-11 21:43:24 +0300

    Delete index.php

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php 
-// Ne pas copier le fichier dnd.php (Do Not Delete.php) pour le coller ici car , vous serez peut-être amener à réinstaller Ksweb, et si tel est le cas, ce fichier sera écraser. Il vous suffira alors juste de réécrire cette ligne de code juste.
-require('dnd.php');
-?>
